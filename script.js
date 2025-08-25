eb9f8def6a0275e77ffcc330366f825e09bb03c0
lafotna <qwaszx89983@gmail.com>
2025-08-25 13:43:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const leftBtn = slider.querySelector('.slider-btn.left');
         const rightBtn = slider.querySelector('.slider-btn.right');
         let scrollPosition = 0;
-        const cardWidth = -220; // عرض البطاقة + الهامش
+        const cardWidth =220; // عرض البطاقة + الهامش
 
         leftBtn.addEventListener('click', () => {
             scrollPosition += cardWidth;
@@ -79,3 +79,4 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
